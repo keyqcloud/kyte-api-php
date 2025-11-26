4940f9437cc72c5fcb52784de96731433a372bdb
Kenneth Hough <kenneth@keyqcloud.com>
2025-11-25 23:56:08 -0600

    Update changelog and version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+## 1.0.1
+* Kyte.JS v1.2.23
+
 ## 1.0.0
 * initial release
--- src/Api/Version.php
+++ src/Api/Version.php
@@ -5,7 +5,7 @@ class Version
 {
     const MAJOR=1;
     const MINOR=0;
-    const PATCH=0;
+    const PATCH=1;
 
     public static function get()
     {
